976a4d1641aa27792f96c6236980e7daaa650817
barbaselua & Nicole Cody <pair+barbaselua.ncody@devbootcamp.com>
2017-10-06 12:17:51 -0500

    Switch from testing to real app

--- app/controllers/ingredients.rb
+++ app/controllers/ingredients.rb
@@ -1,42 +1,37 @@
 require 'json'
 
 get '/' do
-  erb :'/recipes/testing'
+  erb :'/recipes/index'
 end
 
-# post '/recipes' do
-#   response = finder(params)
-#   data = response.as_json
-#   body_section = data["raw_body"]
-#   json_body = JSON.parse(body_section)
-#   body= json_body[0]
-#   id = body["id"]
-#   title = body["title"]
-#   session[:recipe_title] = title
-#   session[:recipe_id] = id
-#   redirect '/recipes'
-# end
+post '/recipes' do
+  response = finder(params)
+  data = response.as_json
+  body_section = data["raw_body"]
+  json_body = JSON.parse(body_section)
+  body= json_body[0]
+  id = body["id"]
+  title = body["title"]
+  session[:recipe_title] = title
+  session[:recipe_id] = id
+  redirect '/recipes'
+end
 
-# get '/recipes' do
-#   p session[:recipe_id]
-#   @title = session[:recipe_title]
-#   response = recipe_id_finder(session[:recipe_id])
-#   p response
-#   data = response.as_json
-#   body_section = data["raw_body"]
-#   body = JSON.parse(body_section)
-#   p "JSON BODY"
-#   p body
+get '/recipes' do
+  @title = session[:recipe_title]
+  response = recipe_id_finder(session[:recipe_id])
+  data = response.as_json
+  body_section = data["raw_body"]
+  body = JSON.parse(body_section)
+  @instructions = body["instructions"]
+  extended_ingredients = body["extendedIngredients"]
+  @ingredients = []
+  extended_ingredients.each do |ingredient|
+    @ingredients << ingredient["originalString"]
+  end
 
-#   @instructions = body["instructions"]
-#   extended_ingredients = body["extendedIngredients"]
-#   @ingredients = []
-#   extended_ingredients.each do |ingredient|
-#     @ingredients << ingredient["originalString"]
-#   end
-
-#   erb :'/recipes/show'
-# end
+  erb :'/recipes/show'
+end
 
 
 
